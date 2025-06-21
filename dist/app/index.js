5ae410ca993387470d67eab157b3780a4e743f01
Brandi <pvtpyro@yahoo.com>
2025-06-21 15:58:34 -0700

    implement architecture

--- dist/app/index.js
+++ dist/app/index.js
@@ -1,15 +1,13 @@
 import express from "express";
+import { middlewareGetMetrics, middlewareLogResponses, middlewareMetricsInc, middlewareResetMetrics } from "./middleware.js";
+import { handlerReadiness } from "./api/readiness.js";
 const app = express();
 const PORT = 8080;
-app.use("/app", express.static("./src/app"));
-// healthz
-function handlerReadiness(req, res) {
-    res.type("text/plain; charset=utf-8");
-    res.status(200);
-    res.send("OK");
-}
-;
+app.use(middlewareLogResponses);
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", middlewareGetMetrics, handlerReadiness);
+app.get("/reset", middlewareResetMetrics, handlerReadiness);
 app.listen(PORT, () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
--- src/app/index.ts
+++ src/app/index.ts
@@ -1,12 +1,18 @@
 import express from "express";
-import { handlerReadiness } from "src/api/readiness";
+import { middlewareGetMetrics, middlewareLogResponses, middlewareMetricsInc, middlewareResetMetrics } from "./middleware.js";
+import { handlerReadiness } from "./api/readiness.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
+app.use("/app", middlewareMetricsInc, express.static("./src/app"));
+
 
 app.get("/healthz", handlerReadiness);
+app.get("/metrics", middlewareGetMetrics, handlerReadiness);
+app.get("/reset", middlewareResetMetrics, handlerReadiness);
+
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
