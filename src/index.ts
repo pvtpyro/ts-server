8082dc3d81b50970b5804ca0932674098f3a93f3
Brandi <pvtpyro@yahoo.com>
2025-06-21 20:59:12 -0700

    deny GET access to reset

--- src/index.ts
+++ src/index.ts
@@ -14,7 +14,10 @@ app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 app.get("/api/healthz", handlerReadiness);
 
 app.get("/admin/metrics", handlerMetrics);
-app.get("/admin/reset", handlerReset);
+app.post("/admin/reset", handlerReset);
+// app.get('/admin/reset', (req, res) => {
+//     res.status(404).send('Page not found.');
+// });
 
 app.listen(PORT, () => {
 	console.log(`Server is running at http://localhost:${PORT}`);
