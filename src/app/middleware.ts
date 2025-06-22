31d024539349d3c4476e37a69ec9c14d22aa0611
Brandi <pvtpyro@yahoo.com>
2025-06-21 17:26:08 -0700

    ignore stupid chrome plugin errors

--- src/app/middleware.ts
+++ src/app/middleware.ts
@@ -7,6 +7,10 @@ export function middlewareLogResponses(req: Request, res: Response, next: NextFu
     res.on("finish", () => {
         const status = res.statusCode;
 
+        if(req.url == "/.well-known/appspecific/com.chrome.devtools.json") {
+            return;
+        }
+
         if(status >= 300) {
             console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${status}`)
         }
