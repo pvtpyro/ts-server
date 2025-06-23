4b29a90c6a071fa5861d7b4e44f487e7b825f84f
Brandi <pvtpyro@yahoo.com>
2025-06-23 12:36:17 -0700

    add error validating middleware to routes

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -1,5 +1,6 @@
 import type { Request, Response, NextFunction } from "express";
 import { config } from "../config.js";
+import { respondWithError } from "./json.js";
 
 export function middlewareLogResponse(req: Request, res: Response, next: NextFunction) {
     res.on("finish", () => {
@@ -20,4 +21,9 @@ export function middlewareLogResponse(req: Request, res: Response, next: NextFun
 export function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
     config.fileServerHits += 1;
     next();
+}
+
+export function middlewareErrorHandler(err: Error, req: Request, res: Response, next: NextFunction) {
+    console.error(err.message);
+    respondWithError(res, 500, 'Something went wrong on our end');
 }
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
+import { middlewareErrorHandler, middlewareLogResponse, middlewareMetricsInc } from "./api/middleware.js";
 import { handlerReadiness } from "./api/readiness.js";
 import { handlerMetrics } from "./api/metrics.js";
 import { handlerReset } from "./api/reset.js";
@@ -12,18 +12,26 @@ const PORT = 8080;
 // Middleware to parse JSON bodies
 app.use(express.json());
 
-app.use(middlewareLogResponse);
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 
-app.get("/api/healthz", handlerReadiness);
+app.get("/api/healthz", (req, res, next) => {
+  	Promise.resolve(handlerReadiness(req, res)).catch(next);
+});
 
-app.post("/api/validate_chirp", handlerValidate)
+app.post("/api/validate_chirp", (req, res, next) => {
+  	Promise.resolve(handlerValidate(req, res)).catch(next);
+});
 
-app.get("/admin/metrics", handlerMetrics);
-app.post("/admin/reset", handlerReset);
+app.get("/admin/metrics", (req, res, next) => {
+  	Promise.resolve(handlerMetrics(req, res)).catch(next);
+});
+app.post("/admin/reset", (req, res, next) => {
+  	Promise.resolve(handlerReset(req, res)).catch(next);
+});
 
 
+app.use(middlewareLogResponse, middlewareErrorHandler);
 app.listen(PORT, () => {
 	console.log(`Server is running at http://localhost:${PORT}`);
 });
